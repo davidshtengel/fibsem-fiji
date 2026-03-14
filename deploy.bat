b3f7e5afc873866471c7a63838e50790fb8733e8
David Shtengel <szb3rh@virginia.edu>
2026-03-14 13:17:32 -0400

    removed whitespace that affected ability to copy new jar into Fiji\plugins

--- deploy.bat
+++ deploy.bat
@@ -17,7 +17,7 @@ del "%FIJI_PLUGINS%\fib_sem-*.jar" 2>nul
 echo Installing ...
 rem Use findstr /r and regex to copy just the fib_sem-#.#.#.jar into the Fiji\plugins directory specified above
 for %%f in (target\fib_sem-*.jar) do (
-    echo %%~nxf | findstr /r "^fib_sem-[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*[.]jar$" >nul && copy /y "%%f" "%FIJI_PLUGINS%\" >nul
+    echo %%~nxf| findstr /r "^fib_sem-[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*[.]jar$" >nul && copy /y "%%f" "%FIJI_PLUGINS%\" >nul
 )
 
 echo Done! Restart Fiji.
