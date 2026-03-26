7e4d9e45afe3922acab487dfdd4ae78e8dae202f
David Shtengel <szb3rh@virginia.edu>
2026-03-26 15:44:33 -0400

    Fixed issue (didn't apply cyan-red swap to individual gaussian plots)

--- src/main/java/com/shtengel/fib_sem/util/Col.java
+++ src/main/java/com/shtengel/fib_sem/util/Col.java
@@ -9,16 +9,16 @@ public class Col {
 	public static final Color THR_MAX = Color.decode("#CC0000");
 
 	// Plot curves & data
-	public static final Color PDF = Color.decode("#0000CC");
-	public static final Color CDF = Color.decode("#CC00CC");
 	public static final Color DATA = Color.decode("#0000CC");
+	public static final Color PDF = Color.decode("#0000CC");
+	public static final Color HIST = Color.decode("#0000CC");
+	public static final Color CDF = Color.decode("#CC00CC");
 	public static final Color LINEAR_FIT = Color.decode("#AA00FF");
 	public static final Color CONST_FIT = Color.decode("#00DD00");
 	public static final Color GAUSS_FIT = Color.decode("#CC00CC");
-	public static final Color GAUSS_1 = Color.decode("#CC0000");
-	public static final Color GAUSS_2 = Color.decode("#00CCCC");
-	public static final Color HIST = Color.decode("#0000CC");
-	public static final Color MEAN_LINE = Color.decode("#CC0000");
+	public static final Color GAUSS_1 = Color.decode("#00CCCC");
+	public static final Color GAUSS_2 = Color.decode("#CC0000");
+	public static final Color MEAN_LINE = Color.decode("#AA00FF");
 
 	// Overlay / image annotations
 	public static final Color EDGE_STROKE = Color.decode("#CCCC00");
